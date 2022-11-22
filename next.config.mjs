47e00634008c06d7489d500f1d22694e1160f662
Erik Solen <erik@solen.dev>
2022-11-22 13:12:30 -0500

    get warning local and export both working

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 const basePath = '/mwra-vis'
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   basePath,
   env: {
     NEXT_PUBLIC_BASE: basePath,
   },
-  async exportPathMap() {
+  async exportPathMap(__unused, { dev }) {
     const paths = {
       '/': {
         page: '/',
         // without the following query, was getting a bunch of warn noise locally
         // warn  - Url '/' defines a query parameter '__nextDefaultLocale' that is missing in exportPathMap
         // warn  - Url '/' defines a query parameter '__nextDataReq' that is missing in exportPathMap
-        query: { __nextDefaultLocale: 'en', __nextDataReq: null },
+        ...(dev
+          ? { query: { __nextDefaultLocale: 'en', __nextDataReq: null } }
+          : {}),
       },
     }
     return paths
